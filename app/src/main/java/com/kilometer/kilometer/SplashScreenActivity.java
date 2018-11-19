092825724d20e9d7f3cd08eb80e69ba175d79a12
Nishad Khan <mknishad1@ymail.com>
2018-11-19 22:38:25 +0600

    add splash screen

---
+++ app/src/main/java/com/kilometer/kilometer/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package com.kilometer.kilometer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(getApplicationContext(), MapsActivity.class));
+        finish();
+    }
+}
